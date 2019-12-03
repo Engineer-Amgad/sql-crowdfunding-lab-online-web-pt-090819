9e2c90e5370237b35990a304858a0d75997c6ff2
Amgad Malik <amgadmalik@yahoo.com>
2019-12-03 02:04:33 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -12,8 +12,7 @@ def selects_the_titles_of_all_projects_and_their_pledge_amounts_alphabetized_by_
   FROM projects 
   INNER JOIN pledges 
   ON projects.id = pledges.project_id
-  GROUP BY projects.title
-  ORDER BY projects.title ASC;"
+  GROUP BY projects.title;"
 end
 
 def selects_the_user_name_age_and_pledge_amount_for_all_pledges_alphabetized_by_name
@@ -21,8 +20,7 @@ def selects_the_user_name_age_and_pledge_amount_for_all_pledges_alphabetized_by_
   FROM users
   INNER JOIN pledges
   ON users.id = pledges.user_id
-  GROUP BY users.name
-  ORDER BY users.name ASC;"
+  GROUP BY users.name;"
 end
 
 def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_funding_goal
